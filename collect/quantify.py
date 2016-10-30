593b9e27666cdda6cb621f2a0a04d030b43facab
Sander Nemvalts <snemvalts@gmail.com>
2016-10-30 17:20:18 +0200

    Quantify average and letters

--- collect/quantify.py
+++ collect/quantify.py
@@ -1,4 +1,6 @@
 import sys
+from statistics import mode
+import string
 
 
 def distribution(dataset):
@@ -8,45 +10,107 @@ def distribution(dataset):
 			if(j in labels):
 				labels[j] += 1
 			elif(j.upper() == j):
-				labels[j] = 0
+				labels[j] = 1
 
 	print("DISTRIBUTION")
 	for key in labels:
 		print("================")
 		print("Key: "+key)
-		print("Amount: "+str(labels[key]))
+		print("Tweets with: "+str(labels[key]))
 		print("Percentage in tweets: "+str(labels[key]/len(dataset)*100)+"%")
-	print("================")
 
-def exclusivity(dataset):
+
+def average(dataset):
 	labels = {}
 	for i in dataset:
 		for j in i[1]:
 			if(j in labels):
-				labels[j] += 1
+				labels[j] += i[1].count(j)
 			elif(j.upper() == j):
-				labels[j] = 0
+				labels[j] = 1
 
-	print("DISTRIBUTION")
+	print("AVERAGE")
 	for key in labels:
 		print("================")
 		print("Key: "+key)
 		print("Amount: "+str(labels[key]))
-		print("Percentage in tweets: "+str(labels[key]/len(dataset)*100)+"%")
-	print("================")
+		print("Average amount in tweets: "+str(labels[key]/len(dataset)))
+
+
+def letters(dataset):
+	positive = ["JOY","LOVE"]
+	negative = ["SAD","FEAR","CONTEMPT","ANGER"]
+	max_diff = max_diff_letter = 0
+	print("LETTERS")
+	for letter in list(string.ascii_lowercase):
+		pos_count = neg_count = pos_letter = neg_letter = 0
+		print("Working on: "+letter)
+		for i in dataset:
+			try:
+				common = mode(i[1])
+			except:
+				continue
+			if(common in positive):
+				pos_count += 1
+				pos_letter += i[0].lower().count(letter)
+			elif(common in negative):
+				neg_count += 1
+				neg_letter += i[0].lower().count(letter)
+		diff = abs((pos_letter/pos_count)-(neg_letter/neg_count))
+		if(diff > max_diff):
+			max_diff = diff
+			max_diff_letter = letter
+
+	print("Max_diff: "+str(max_diff))
+	print("Max_diff_letter: "+max_diff_letter)
+'''
+	print("AMOUNT OF V'S")
+	print("Average amount of positive v's: "+str(pos_v/pos_count))
+	print("Average amount of negative v's: "+str(neg_v/neg_count))
+	print("Erroneous modes: "+str(error))'''
+'''
+def exclusivity(dataset):
+	labels = {}
+	for i in dataset:
+		for j in i[1]:
+			if(j.upper() == j and j not in labels):
+				labels[j] = 1
+	labels = {i:labels for i in labels}
 	
+	#remove occurences of self in subdictionary
+	#not good at pythonic oneliners
+
+	
+	for i in dataset:
+		for j in i[1]:
+			if(j in list(labels.keys())):
+				for k in labels[j]:
+						if(j!=k and j in i[1] and k in i[1]):
+							labels[j][k]+=1
+
+	print(labels)
+	#print("DISTRIBUTION")
+	#for key in labels:
+	#	print("================")
+	#	print("Key: "+key)
+	#	print("Amount: "+str(labels[key]))
+	#	print("Percentage in tweets: "+str(labels[key]/len(dataset)*100)+"%")
+	#print("================")
+'''
+
+
 def main(functions):
 	if(not functions):
-		functions = ["distribution"]
+		functions = ["distribution","average","letters"]
 	dataset = open("data/tweets.processed.log","r").read().splitlines()
 	dataset = [i.split(" ;; ") for i in dataset]
 	for i,v in enumerate(dataset):
 		dataset[i][1] = dataset[i][1].split(",")
 	for i in functions:
-		print("Working on: "+i)
 		if(globals()[i]):
 			globals()[i](dataset)
-
+			print("================")
+			print("================")
 
 
 if __name__ == "__main__":
